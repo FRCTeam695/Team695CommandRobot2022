7da31569351cebc0aa677a0e58e01c00633cdafc
Vyas <trio.sav@gmail.com>
2022-02-21 19:18:27 -0500

    Make intake lift motor reset in robot init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,7 +41,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-
+    IntakeLiftSubsystem.m_IntakeLiftMotor.setSelectedSensorPosition(0);
     
   }
 
