25894406e5195b1081f822a00bb7e711cb46807e
Vyas <trio.sav@gmail.com>
2022-04-02 13:27:49 -0400

    Recharacterize drivetrain after Buckeye (also note kP value)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,14 +29,14 @@ public class Constants {
         public static final int kRightMotor1Port = 13;
         public static final int kRightMotor2Port = 14;
 
-        public static final double ksVolts = 0.69542;
-        public static final double kvVoltSecondsPerMeter = 2.3779;
-        public static final double kaVoltSecondsSquaredPerMeter = 0.36794;
+        public static final double ksVolts = 0.6851;
+        public static final double kvVoltSecondsPerMeter = 2.3181;
+        public static final double kaVoltSecondsSquaredPerMeter = 0.3326;
 
-        public static final double kPDriveVelMetersPerSec = 6.0;
+        public static final double kPDriveVelMetersPerSec = 3.2599;
         
         //If changed update pathweaver and rebuild paths
-        public static final double kTrackwidthMeters = 0.54605;
+        public static final double kTrackwidthMeters = 0.54183;
         
         // 0.15 meters - diameter of the wheel; 2048 units per rotation - Talon FX; 8.45:1 Gearbox Ratio;
         public static final double kMetersRobotTravelPerEncoderCount = .15 * 3.14 / (2048 * 10.71);   
